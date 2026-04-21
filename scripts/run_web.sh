638e629a75f1a5763c47531562b87f6377aceb54
ozand <ozand.ru@gmail.com>
2026-04-21 12:35:19 +0300

    Document canonical dashboard runtime discipline

--- scripts/run_web.sh
+++ scripts/run_web.sh
@@ -5,4 +5,4 @@ ROOT="/home/ozand/herkoot/Projects/nanobot-ops-dashboard"
 cd "$ROOT"
 export PYTHONPATH=src
 : "${NANOBOT_EEEPC_SUDO_PASSWORD:?Set NANOBOT_EEEPC_SUDO_PASSWORD first}"
-exec python3 -m nanobot_ops_dashboard serve --host 127.0.0.1 --port 8787
+exec python3 -m nanobot_ops_dashboard serve --host 0.0.0.0 --port 8787
